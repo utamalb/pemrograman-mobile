69627782f9c411f4859d0e267f7ae39e6e1197a9
utamalb <123740870+utamalb@users.noreply.github.com>
2024-07-01 15:41:01 +0700

    home.js

--- home.js
+++ home.js
@@ -1,13 +1,31 @@
+// components/Home.js
 import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
+import Kontak from './kontak';
 
 const Home = ({ navigation }) => {
+  const handlePress = (gambar, judul, telpon) => {
+    navigation.navigate('About', { gambar, judul, telpon });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Kontak
+        gambar={require('../assets/snack-icon.png')}
+        judul="M Utama"
+      />
       <Button
-        title='Go to About'
-        onPress={() => navigation.navigate('About')}
+        title="Deskripsi"
+        onPress={() => handlePress(require('../assets/snack-icon.png'), 'M Utama', '089630461568')}
+      />
+
+      <Kontak
+        gambar={require('../assets/snack-icon.png')}
+        judul="Kamil"
+      />
+      <Button
+        title="Deskripsi"
+        onPress={() => handlePress(require('../assets/snack-icon.png'), 'Kamil', '085746551233')}
       />
     </View>
   );
